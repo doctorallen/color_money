56758dd42c7d499dcee2e2ad0a7110e86cb084a9
David Allen <drallen1@plymouth.edu>
2012-10-16 20:57:02 +0000

    stuff

--- index.php
+++ index.php
@@ -1,31 +1,19 @@
 <html>
+<head>
 <script src="js/jquery-1.7.2.min.js"></script>
 <script src="js/jquery.xcolor.js"></script>
-<div id="color1" style="width: 50px; height:50px;"></div>
-<div id="color1g" style="width: 50px; height:50px;"></div>
-<div id="color2" style="width: 50px; height:50px;"></div>
-<div id="color2g" style="width: 50px; height:50px;"></div>
-<div id="color3" style="width: 50px; height:50px;"></div>
-<div id="color3g" style="width: 50px; height:50px;"></div>
-<div id="color4" style="width: 50px; height:50px;"></div>
-<div id="color4g" style="width: 50px; height:50px;"></div>
-<input type="text" name="color" id="color">
+</head>
+<body>
+<input id="colorPicker" type="text"></input>
+	<div id="colors"></div>
 <script type="text/javascript">
-	var color = $.xcolor.analogous("#0070c0");
-	$('#color1').css("background-color", color[0].getHex()).html(color[0].getHex());
-	$('#color2').css("background-color", color[1].getHex()).html(color[1].getHex());
-	$('#color3').css("background-color", color[2].getHex()).html(color[2].getHex());
-	$('#color4').css("background-color", color[3].getHex()).html(color[3].getHex());
+	var color = $.xcolor.analogous("#0070c0", 30, 30);
 	var gradient_colors = new Array();
 	$.each(color, function(index, value){
-		console.log(value);
 		gradient_colors.push($.xcolor.lighten(value.getHex())); 
+		$('#colors').append('<div style="background-color:'+gradient_colors[index].getHex()+'; height: 100px; width: 100px; float: left; margin-right: 5px; color: #fff;">'+gradient_colors[index].getName()+'</div>');
 	});
 
-	$('#color1g').css("background-color", gradient_colors[0]).html(gradient_colors[0]);
-	$('#color2g').css("background-color", gradient_colors[1]).html(gradient_colors[1]);
-	$('#color3g').css("background-color", gradient_colors[2]).html(gradient_colors[2]);
-	$('#color4g').css("background-color", gradient_colors[3]).html(gradient_colors[3]);
-	console.log(color);
 </script>
+</body>
 </html>
